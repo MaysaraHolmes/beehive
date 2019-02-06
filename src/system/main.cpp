afe1a64c56d7844dfa9fce428f6a443555d9619b
Trine Olsen <2420036o@student.gla.ac.uk>
2019-02-06 18:22:30 +0100

    add print

--- src/system/main.cpp
+++ src/system/main.cpp
@@ -20,21 +20,20 @@ int main(){
   TemperatureAndHumidity th1(I2C_PORT1, ADDR_TEMP_AND_HUM);
   int i = th1.writeI2C();
   th1.readI2C(global_buffer);
-  std::cout << "status: " << th1.getStatus() << std::endl;
-  std::cout << "temp: " << th1.getTemp() << std::endl;
-  std::cout << "humidity: " << th1.getHum() << std::endl;
+  //std::cout << "status: " << th1.getStatus() << std::endl;
+  //std::cout << "temp: " << th1.getTemp() << std::endl;
+  //std::cout << "humidity: " << th1.getHum() << std::endl;
 
-  std::bitset<8> b;
-  b[7] = 0;
-  b[6] = 1;
-  b[5] = 0;
-  b[4] = 1;
-  b[3] = 0;
-  b[2] = 1;
-  b[1] = 0;
-  b[0] = 0;
-
-  std::cout<< "testing bitset: " << b << std::endl;
+  //std::bitset<8> b;
+  //b[7] = 0;
+  //b[6] = 1;
+  //b[5] = 0;
+  //b[4] = 1;
+  //b[3] = 0;
+  //b[2] = 1;
+  //b[1] = 0;
+  //b[0] = 0;
+  //std::cout<< "testing bitset: " << b << std::endl;
 
   //std::cout << "unsinged int: " << th1.getStatus(b) << std::endl;
 
