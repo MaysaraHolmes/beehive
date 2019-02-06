47143997a4e8f7510af5f0ecfa39bac86e5520f4
Trine Olsen <2420036o@student.gla.ac.uk>
2019-02-06 19:06:36 +0100

    its working

--- src/system/I2C.cpp
+++ src/system/I2C.cpp
@@ -41,12 +41,12 @@ void I2C::readI2C(int bytesToRead, unsigned char* global_buffer){
 	{
 		//printf("\nData size buffer read: %d\n", sizeof(buffer));
 		//printf("Data read: %s\n", buffer);
-		std::cout << std::hex << buffer << '\n';
-		std::cout  << buffer << '\n';
-		std::bitset<8> bitset1(buffer[0]);
-		std::bitset<8> bitset2(buffer[1]);
-		std::bitset<8> bitset3(buffer[2]);
-		std::bitset<8> bitset4(buffer[3]);
+		//std::cout << std::hex << buffer << '\n';
+		//std::cout  << buffer << '\n';
+		std::bitset<8> bitset1(global_buffer[0]);
+		std::bitset<8> bitset2(global_buffer[1]);
+		std::bitset<8> bitset3(global_buffer[2]);
+		std::bitset<8> bitset4(global_buffer[3]);
 		std::cout << "bitset1: " << bitset1 << std::endl;
 		std::cout << "bitset2: " << bitset2 << std::endl;
 		std::cout << "bitset3: " << bitset3 << std::endl;
