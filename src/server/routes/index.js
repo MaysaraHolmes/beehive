a09a758919f731e7b309b26bd693971fb5f2cbdb
MaysaraHolmes <maysaradeveloper@gmail.com>
2019-02-27 17:01:41 +0000

    rendering data

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -18,7 +18,29 @@ const readings = {
 router.get('/', function(req, res) {
     sql.getData().then((result)=>{
         console.log("done reading data");
-        res.render('index', {data: result[0], title:"BeeHive" });
+        console.log(result);
+        var recentData = result.length==0 ? {} : result[result.length-1];
+        
+        var otemp = [];
+        var itemp = [];
+        var ohum = [];
+        var ihum  = [];
+    
+        for( var i = 0;i<result.length;i++){
+            console.log(result[i]);
+            otemp.push({x:((i+1)*10),y:result[i].otemp});
+            itemp.push({x:((i+1)*10),y:result[i].itemp});
+            ohum.push({x:((i+1)*10),y:result[i].ohum});
+            ihum.push({x:((i+1)*10),y:result[i].ihum});
+        }
+    
+        res.render('index', {recentData:recentData,
+            otemp: encodeURIComponent(JSON.stringify(otemp)),
+            itemp:encodeURIComponent(JSON.stringify(itemp)),
+            ihum:encodeURIComponent(JSON.stringify(ihum)),
+            ohum:encodeURIComponent(JSON.stringify(ohum)),
+            title:"BeeHive" });
+    
     }).catch((err)=>{
         console.log(err);
     });
