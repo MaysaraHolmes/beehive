c07b6c3a3065508b126f2ae6ea83f5365bc1fc9a
Trine Olsen <2420036o@student.gla.ac.uk>
2019-03-13 18:09:46 +0100

    add boost/asio udp code

--- src/system/CppTimer.cpp
+++ src/system/CppTimer.cpp
@@ -1,6 +1,15 @@
 
 #include "CppTimer.h"
 
+
+
+
+#include "boost/asio.hpp"
+using namespace boost::asio;
+
+
+
+
 DemoTimer1::DemoTimer1(){
 	this->r = new ReadI2CDevices();
 }
@@ -11,17 +20,26 @@ void DemoTimer1::timerEvent() {
 	fprintf(stdout,"Buh! ");
 	fflush(stdout);
 
-	
-	r->writeAll();
+	io_service io_service;
+	ip::udp::socket socket(io_service);
+	ip::udp::endpoint remote_endpoint;
 
-	std::thread sensorThread(&ReadI2CDevices::readAll, r);
-	sensorThread.join();
+	r->writeAll();
+	r->readAll();
+
+
+	socket.open(ip::udp::v4());
+	remote_endpoint = ip::udp::endpoint(ip::address::from_string("192.168.0.4"), 9000);
+	boost::system::error_code err;
+	socket.send_to(buffer("Jane Doe", 8), remote_endpoint, 0, err);
+	socket.close();
+
 
 
 }
 
 DemoTimer1::~DemoTimer1(){
 	delete this->r;
-	
+
 
 }
