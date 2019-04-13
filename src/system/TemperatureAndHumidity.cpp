3333832c2995ccd5ef1f30f421f235ee0c60b765
Trine Olsen <2420036o@student.gla.ac.uk>
2019-04-13 20:27:08 +0200

    doxygen comments Temperatur&humidity

--- src/system/TemperatureAndHumidity.cpp
+++ src/system/TemperatureAndHumidity.cpp
@@ -1,41 +1,33 @@
 #include "TemperatureAndHumidity.hpp"
 
 
-
 TemperatureAndHumidity::TemperatureAndHumidity(char* portI2C, int addrI2C): Sensor(4,2, portI2C, addrI2C){
 }
-
 
 int TemperatureAndHumidity::writeI2C(){
   return bus->writeI2C(this->bytesToWrite);
 }
 
-
 void TemperatureAndHumidity::readI2C(unsigned char* global_buffer){
   bus->readI2C(this->bytesToRead, global_buffer);
   int nbOfBytes = strlen((char*)global_buffer);
 
-  //seperate the different bits read
   this->status = ((uint8_t) (global_buffer[0] & 0xC0) >> 6);
   this->humidity = ((uint16_t) (global_buffer[0] & 0x3f) << 8)  | ((uint16_t) global_buffer[1] );
   this->temperature = (((uint16_t) (global_buffer[2]) << 8)  | ((uint16_t) (global_buffer[3] & 0xFC) )) >>2;
 }
 
-
 unsigned int TemperatureAndHumidity::getStatus() {
   return (unsigned int)this->status;
 }
-
 
 double TemperatureAndHumidity::getTemp() {
   return (double)(( ((unsigned int)this->temperature) / (pow(2,14) - 2)) * 165 ) - 40;
 }
 
-
 double TemperatureAndHumidity::getHum() {
   return (double)(this->humidity /(pow(2,14) - 2)) * 100 ;
 }
-
 
 TemperatureAndHumidity::~TemperatureAndHumidity(){
 }
--- src/system/TemperatureAndHumidity.hpp
+++ src/system/TemperatureAndHumidity.hpp
@@ -1,10 +1,14 @@
+//! A  class for the HIH6131 Temperatur & Humidity sensor
+/*!
+	 Handling how to communicate with the sensor,
+	 and converting the sensor measurements to human readable values
+*/
+
 #ifndef TEMPERATUREANDHUMIDITY_HPP
 #define TEMPERATUREANDHUMIDITY_HPP
 
-
 #include "Sensor.hpp"
 #include <stdio.h>
-#include <iostream>
 #include <cstring>  // strlen()
 #include <string>
 #include <cmath>
@@ -12,19 +16,60 @@
 
 class TemperatureAndHumidity: public Sensor{
 
-private:
+	private:
+		//! A private variable.
+		/*!
+			Holding the newest status-bit reading from the sensor
+		*/
 		uint8_t status;
+		//! A private variable.
+		/*!
+			Holding the newest humidity-bit reading from the sensor
+		*/
 		uint16_t humidity;
+		//! A private variable.
+		/*!
+			Holding the newest temperature-bit reading from the sensor
+		*/
 		uint16_t temperature;
 
-public:
+	public:
+		//! A constructor.
+		/*!
+			Call the Sensor constructor
+			\param portI2C a char pointer holding the which I2C bus on the Raspberry the peripheral is connected to.
+			\param addrI2C an integer holding the address to the peripheral.
+		*/
     TemperatureAndHumidity(char* portI2C, int addrI2C);
+		//! Call the I2C-object read function
+		/*!
+			Get the data read form the sensor and split the data into status,
+			humidity and temperatur bits and saves them to the corresponding private variable.
+			\param global_buffer an unsigned char pointer where the data read is saved.
+		*/
     void readI2C(unsigned char* global_buffer);
-    int writeI2C();
+		//! Call the I2C-object write function
+		int writeI2C();
+		//! Convert the status bits into a integer from 0-3
+		/*!
+			\return The status of the sensor.
+		*/
     unsigned int getStatus();
+		//! Convert the temperature bits into degrees
+		/*!
+			\return The temperature in degrees.
+		*/
     double getTemp();
+		//! Convert the humidity bits into %RH
+		/*!
+			\return The humidity in %RH.
+		*/
     double getHum();
 
+		//! A destructor.
+    /*!
+      Empty, can be used if more functions are integrated
+    */
     ~TemperatureAndHumidity();
 };
 
