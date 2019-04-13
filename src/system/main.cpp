4441abafa764e9325f34603d254052a337109810
Trine Olsen <2420036o@student.gla.ac.uk>
2019-04-13 23:14:31 +0200

    doxygen comments in main

--- src/system/main.cpp
+++ src/system/main.cpp
@@ -1,46 +1,24 @@
+//! Project start
+/*!
+   Create a Fan object so the interrupt bin is set to the default value
+   (using the default gpio pin in the Fan constructor)
+   Create a TimerEvent object to start the timer which trigger the sensor-readings.
+*/
 
-#include "ReadI2CDevices.hpp"
-#include "Sensor.hpp"
-#include <iostream>
-#include <stdio.h>
-#include <wiringPi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "CppTimer.h"
+#include "TimerEvent.hpp"
 #include "Fan.hpp"
-
-#include <unistd.h> //NOTE remove?
 
 
 int main(int argc, const char* argv[] ){
-
-
-
-  //TESTING FAN NOTE remove??
   Fan* f = new Fan();
-  std::cout << "start fan" << std::endl;
-  f->setPwm(1023);
-  unsigned int microseconds = 5000000;
-  usleep(microseconds);
-  std::cout << "stop fan" << std::endl;
   f->stop();
 
-
-
-  DemoTimer1 demoTimer1;
-	demoTimer1.start(2000000000); //every 2sec
-
-//NOTE remove??
-wiringPiSetupGpio();
-pinMode(22,INPUT);
-int status = digitalRead(22);
-printf("Pin input = %d\n", status);
+  TimerEvent TimerEvent;
+	TimerEvent.start(2000000000);
 
   do {
 		sleep(1);
 	} while (1);
 
-
-return 0;
-
+  return 0;
 }
